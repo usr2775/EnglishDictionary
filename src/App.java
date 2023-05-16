75c018bf0a3079ffa7e8311b5794ef2613177d53
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-16 08:50:05 +0100

    feaat(app): realizar los casos para el menu

--- src/App.java
+++ src/App.java
@@ -23,6 +23,47 @@ public class App {
             System.out.print("Seleccione una opción: ");
             opcion = sc.nextInt();
             sc.nextLine();
-          
+            switch (opcion) {
+                case 1:
+                    System.out.print("Introduzca la palabra que desea añadir: ");
+                    String palabraAgregar = sc.nextLine();
+                    diccionario.agregarPalabra(palabraAgregar);
+                    System.out.println("La palabra se ha añadido correctamente.");
+                    break;
+                case 2:
+                    System.out.println("Introduce la alabra que desea eliminar:");
+                    String palabraEliminar = sc.nextLine();
+                    if (diccionario.eliminarPalabra(palabraEliminar)) {
+                        System.out.println("La palabra se ha eliminado correctamente.");
+                    } else {
+                        System.out.println("La palabra no estaba almacenada.");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Introduzca la palabra que desea buscar: ");
+                    String palabraBuscar = sc.nextLine();
+                    if (diccionario.existePalabra(palabraBuscar)) {
+                        System.out.println("La palabra" + palabraBuscar + " se ha encontrado.");
+                    } else {
+                        System.out.println("La palabra" + palabraBuscar + " no se ha encontrado.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Iniciales disponibles: " + diccionario.obtenerIniciales()));
+                    break;
+                case 5:
+                    System.out.print("Introduzca la letra de la inicial que desea buscar: ");
+                    char inicial = sc.nextLine().charAt(0);
+                    System.out.println("Palabras por inicial: " + diccionario.obtenerPalabras(inicial));
+                    break;
+                case 6:
+                    System.out.println("¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        } while (opcion != 6);
     }
 }
